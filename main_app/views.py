7400c4d1547be31146ea50cb08906b3bd93ea775
David Aydin <david.aydin6@gmail.com>
2022-07-13 00:14:04 -0400

    cat model, url and view updates

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Cat
+
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats':cats})
